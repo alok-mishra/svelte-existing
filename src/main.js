453ec2a118aa6d0ed63ee27287abbe92db67a795
Alok Mishra <alok@alokmishra.com>
2022-02-13 23:54:50 -0500

    prop generating output well

--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 import App from './App.svelte';
 
+/*
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+    target: document.body,
+    props: {
+        name: 'world',
+    },
 });
+*/
+
+const app = (component, props) => {
+    new App({
+        target: document.body,
+        props: {
+            name: attempts,
+        },
+    });
+};
 
 export default app;
+
+// export default function (target, props) {
+//     new App({
+//         target,
+//         props,
+//     });
+// }
+
+// export default function (target, props) {
+//     new App({
+//         target,
+//         props,
+//     });
+// }
